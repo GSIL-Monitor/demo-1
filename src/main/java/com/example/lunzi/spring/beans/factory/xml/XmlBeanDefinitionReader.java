9571a2b55a8bdba2bfbcfe9e87699f333c66411f
suosong <903748723@qq.com>
2018-08-11 22:55:28 +0800

    ss

--- src/main/java/com/example/lunzi/spring/beans/factory/xml/XmlBeanDefinitionReader.java
+++ src/main/java/com/example/lunzi/spring/beans/factory/xml/XmlBeanDefinitionReader.java
@@ -8,6 +8,7 @@ import com.example.lunzi.spring.beans.factory.config.TypeStringValue;
 import com.example.lunzi.spring.beans.factory.support.BeanDefinitionRegistry;
 import com.example.lunzi.spring.beans.factory.support.GenericBeanDefinition;
 import com.example.lunzi.spring.beans.factory.BeanDefinitionReader;
+import com.example.lunzi.spring.context.annotation.ClasspathBeanDefinitionScanner;
 import com.example.lunzi.spring.core.io.Resource;
 
 import org.dom4j.Document;
@@ -16,6 +17,7 @@ import org.dom4j.io.SAXReader;
 import org.springframework.context.annotation.Bean;
 
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.util.Iterator;
@@ -42,6 +44,12 @@ public class XmlBeanDefinitionReader implements BeanDefinitionReader {
     private final String PROPERTY_ELEMENT = "property";
     private final String CONSTRUCTOR_ARG = "constructor-arg";
 
+    private final String COMPONENT_SCAN = "component-scan";
+    private final String BASE_PACKAGE_ATTRIBUTE = "base-package";
+
+    private final String DEFAULT_NAMESPACE_URI = "http://www.springframework.org/schema/beans";
+    private final String CONTEXT_NAMESPACE_URI = "http://www.springframework.org/schema/context";
+
     @Override
     public void loadBeanDefinitions(Resource resource) throws BeanDefinitionStoreException {
         //Assert.notNull(path, "xml路径不能为空");
@@ -53,27 +61,69 @@ public class XmlBeanDefinitionReader implements BeanDefinitionReader {
             Element root = document.getRootElement();
             Iterator<Element> it = root.elementIterator();
             while (it.hasNext()) {
-                Element beanEle = it.next();
-                String name = beanEle.attributeValue(NAME_ATTRIBUTE);
-                String className = beanEle.attributeValue(CLASS_ATTRIBUTE);
-                String scope = beanEle.attributeValue(SCOPE_ATTRIBUTE);
-                if (null == scope) scope = "";
-                GenericBeanDefinition definition = new GenericBeanDefinition();
-                definition.setName(name);
-                definition.setBeanClassName(className);
-                definition.setScope(scope);//这里必须用set方法，而不能放到构造函数中去
-                //解析<property>标签
-                parseProperty(beanEle, definition);
-                //解析<constructor-arg>标签
-                parseConstructorArgument(beanEle,definition);
-                //注册
-                this.registry.registerBeanDefinition(name, definition);
+                Element ele = it.next();
+                String nameSpaceUri = ele.getNamespaceURI();
+                if(isDefaultNameSpaceUri(nameSpaceUri)){
+                    parseDefaultElement(ele);
+                }else if(isContextNameSpaceUri(nameSpaceUri)){
+                    parseComponentElement(ele);
+                }
+
+
             }
 
         } catch (Exception e) {
             throw new BeanDefinitionStoreException("xml解析失败", e);
         }
 
+    }
+
+    /**
+     * 解析注解
+     * @param ele
+     */
+    private void parseComponentElement(Element ele) throws IOException {
+        if(ele.getName().equals(COMPONENT_SCAN)){
+            String packageToScan = ele.attributeValue(BASE_PACKAGE_ATTRIBUTE);
+            ClasspathBeanDefinitionScanner scanner = new ClasspathBeanDefinitionScanner(registry);
+            scanner.doScan(packageToScan);
+        }
+
+    }
+
+    /**
+     * 标签前缀是否为context
+     * @param nameSpaceUri
+     * @return
+     */
+    private boolean isContextNameSpaceUri(String nameSpaceUri) {
+        return CONTEXT_NAMESPACE_URI.equals(nameSpaceUri);
+    }
+
+    private void parseDefaultElement(Element ele) {
+        String name = ele.attributeValue(NAME_ATTRIBUTE);
+        String className = ele.attributeValue(CLASS_ATTRIBUTE);
+        String scope = ele.attributeValue(SCOPE_ATTRIBUTE);
+        if (null == scope) scope = "";
+        GenericBeanDefinition definition = new GenericBeanDefinition();
+        definition.setName(name);
+        definition.setBeanClassName(className);
+        definition.setScope(scope);//这里必须用set方法，而不能放到构造函数中去
+        //解析<property>标签
+        parseProperty(ele, definition);
+        //解析<constructor-arg>标签
+        parseConstructorArgument(ele,definition);
+        //注册
+        this.registry.registerBeanDefinition(name, definition);
+    }
+
+    /**
+     * 是否为默认的，也就是 标签前缀为bean
+     * @param nameSpaceUri
+     * @return
+     */
+    private boolean isDefaultNameSpaceUri(String nameSpaceUri) {
+        return DEFAULT_NAMESPACE_URI.equals(nameSpaceUri);
     }
 
     /**
